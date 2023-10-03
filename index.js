b6311347eff0e6eb5ff4dfb07cde5e33f39e4268
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2023-10-03 12:29:39 -0300

    nova

---
+++ index.js
@@ -0,0 +1,7 @@
+let num1 = 4;
+let num2 = 5;
+
+console.log( `A soma do número A: ${num1}  e o numero B:  ${num2} o total é: ${num1 + num2}`);
+console.log( `A subitração do número A: ${num1}  e o numero B:  ${num2} o total é: ${num1 - num2}`);
+console.log( `A multiplicação do número A: ${num1}  vezes o numero B:  ${num2} o total é: ${num1 * num2}`);
+console.log( `A divisão do número A: ${num1}  pelo numero B:  ${num2} o total é: ${num1 / num2}`);
